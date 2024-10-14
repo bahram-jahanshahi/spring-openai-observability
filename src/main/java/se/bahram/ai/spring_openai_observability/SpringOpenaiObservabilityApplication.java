998c992c4b45a239df89d714d933d5b802ce55d7
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2024-10-14 21:34:12 +0200

    Entity is added to the prompt

--- src/main/java/se/bahram/ai/spring_openai_observability/SpringOpenaiObservabilityApplication.java
+++ src/main/java/se/bahram/ai/spring_openai_observability/SpringOpenaiObservabilityApplication.java
@@ -1,13 +1,16 @@
 package se.bahram.ai.spring_openai_observability;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -37,6 +40,7 @@ class JokeController {
 	Map<String, String> joke() {
 		var reply = chatClient
 				.prompt()
+				.advisors(new SimpleLoggerAdvisor())
 				.user("""
       					Tell me a joke. Be concise. Don't say anything except a joke
 						""")
@@ -46,6 +50,26 @@ class JokeController {
 		return Map.of("joke", reply);
 	}
 
+	@GetMapping("/actor-films")
+	ActorFilms actorFilms() {
+		ActorFilms actorFilms = chatClient.prompt()
+				.advisors(new SimpleLoggerAdvisor())
+				.user("Generate the filmography for a random actor.")
+				.call()
+				.entity(ActorFilms.class);
+		return actorFilms;
+	}
 
+	@GetMapping("/actors-films")
+	List<ActorFilms> actorsFilms() {
+		List<ActorFilms> actorFilms = chatClient.prompt()
+				.advisors(new SimpleLoggerAdvisor())
+				.user("Generate the filmography of 5 movies for Tom Hanks and Bill Murray.")
+				.call()
+				.entity(new ParameterizedTypeReference<List<ActorFilms>>() {});
+		return actorFilms;
+	}
 
 }
+
+record ActorFilms(String actor, List<String> movies){}
